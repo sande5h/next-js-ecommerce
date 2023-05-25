d4a5dd3c6cea0646e0af5ebf110cfd1bb3fc89ea
sande5h <bhusalsandesh0@gmail.com>
2023-05-25 19:35:45 +0545

    google auth and admin panel init

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Nav from "@/components/Nav";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  return (
-    <>
-    
-    </>
+  const { data: session } = useSession();
+  if (!session) {
+    return ( <div className="bg-blue-900 w-screen h-screen flex ">
+    <div className="text-center w-full flex items-center justify-center">
+      <button onClick={()=>signIn('google')} className="bg-white p-2 rounded-lg">Login with Google</button>
+    </div>
+  </div>)
+  }
 
 
-  )  
+  return(
+    <div className="bg-blue-900 min-h-screen">
+      <Nav/>
+      Logged in {session.user.email}
+    </div>
+  )
 }
